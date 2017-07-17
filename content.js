8e538785fb716e43e2f9a6b65114b9c30cc31b29
Mike Marion <mike@buildout.com>
2017-07-17 13:38:20 -0500

    Update regex and point to Jira

--- content.js
+++ content.js
@@ -6,9 +6,13 @@
 
   chrome.storage.local.get('inlineLinks', (options) => {
     let title = $title.innerHTML.replace(/(<a[^>]+>|⬆︎|<\/a>)/g, '');
+    // TODO: Bring back multiple matches for the new format
+    // TODO: Format settings in options page? (Also, domain option?)
+    let matches = title.match(/^\s*(\w+-\d+):?/);
 
-    title.match(/[a-zA-Z0-9-]+(?=[\],\s\d#]*\])/g).forEach((tag) => {
-      const url = `https://nextcapital.atlassian.net/browse/${tag}`;
+    if (matches) {
+      let tag = matches[1];
+      const url = `https://buildout.atlassian.net/browse/${tag}`;
       const attrs = `href="${url}" target="_blank"`;
 
       const replacement = options.inlineLinks === false ?
@@ -16,7 +20,7 @@
         `<a ${attrs}>${tag}</a>`;
 
       title = title.replace(tag, replacement);
-    });
+    }
 
     $title.innerHTML = title;
   });
