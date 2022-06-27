fc43a8068eb8e48abfc4f05c1653ad4e919bf77f
Mike Marion <mike@buildout.com>
2022-06-27 16:32:00 -0500

    Upgrade to manifest V3

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
   if (info.status == "complete") {
-    chrome.tabs.executeScript(tabId, { file: 'add-jira-links-to-title.js' });
-    chrome.tabs.executeScript(tabId, { file: 'add-jira-links-to-description.js' });
+    chrome.scripting.executeScript({
+      files: [
+        'add-jira-links-to-title.js',
+        'add-jira-links-to-description.js',
+      ],
+      target: { tabId },
+    });
   }
 });
